c197e10d189befcb62ae7c7bb4224b022617761c
atcherkasov <atcherkasov@hotmail.com>
2020-05-02 19:14:24 +0300

    refactor poly_func

--- source/poly_func/my_poly_functions.py
+++ source/poly_func/my_poly_functions.py
@@ -102,7 +102,7 @@ def mult_global(a: list, b: list, x_size: int, y_pow: int) -> list:
     return ans[-x_size:]
 
 
-def make_equation(arr, xy_equation=True):
+def make_equation_cut(arr, xy_equation=True):
     a, b, c, d, x_size, y_pow = arr
     x = [[1], [0]]
     y = [[1, 0]]
@@ -133,26 +133,21 @@ def make_equation(arr, xy_equation=True):
                            y_pow)[-2 * x_size + 1:]
 
 
-def mult_y(a: list, b: list, max_pow) -> list:
-    ans = [0] * min((len(a) + len(b) - 1), max_pow + 1)
-    iterat_1 = min(len(a), max_pow + 1)
-    ans_len = len(ans)
-    a_len = len(a)
-    b_len = len(b)
-    for i in range(iterat_1):
-        # хотим вычислить коэффициент при y^((len(a) + len(b) - 1) - i)
-        iterat_2 = min(max_pow - i + 1, len(b))
-        for j in range(iterat_2):
-            ans[ans_len - i - j - 1] += a[a_len - i - 1] * b[b_len - j - 1]
+def mult_y(a: list, b: list) -> list:
+    ans = [0] * (len(a) + len(b) - 1)
+    for i in range(len(a)):
+        # хотим вычислить коэффициент при x^((len(a) + len(b) - 1) - i)
+        for j in range(len(b)):
+            ans[i + j] += a[i] * b[j]
     i = 0
-    while i < ans_len and ans[i] == 0:
+    while i < len(ans) and ans[i] == 0:
         i += 1
-    if i == ans_len:
+    if i == len(ans):
         return [0]
     return ans[i:]
 
 
-def mult(a: list, b: list, x_size: int, y_pow: int) -> list:
+def mult(a: list, b: list, size: int) -> list:
     """
     вычисляет произведение двух мнгочленов от двух переменных
     :param a:
@@ -167,43 +162,37 @@ def mult(a: list, b: list, x_size: int, y_pow: int) -> list:
     for i in range(len(a)):
         # хотим вычислить коэффициент при x^((len(a) + len(b) - 1) - i)
         for j in range(len(b)):
-            if (len(a) + len(b) - 1) - x_size <= i + j:
-                ans[i + j] = add_y(ans[i + j], mult_y(a[i], b[j], y_pow))
+            if (len(a) + len(b) - 1) - size <= i + j:
+                ans[i + j] = add_local(ans[i + j], mult_y(a[i], b[j]))
     i = 0
     while i < len(ans) and ans[i] == [0]:
         i += 1
     if i == len(ans):
         return [[0]]
     ans = ans[i:]
-    return ans[-x_size:]
+    return ans[-size:]
 
 
 def make_equation(arr, xy_equation=True):
-    a, b, c, d, x_size, y_pow = arr
+    a, b, c, d, series_size = arr
     x = [[1], [0]]
     y = [[1, 0]]
     minus_one = [[-1]]
     if (xy_equation):
         return mult(x,
-                    add_global(mult(a, b, 2 * x_size + 1, y_pow),
+                    add_global(mult(a, b, 2 * series_size + 1),
                         mult(mult(add_global(y, minus_one),
                                   c,
-                                  2 * x_size + 1,
-                                  y_pow),
+                                  2 * series_size + 1),
                              d,
-                             2 * x_size + 1,
-                             y_pow)),
-                    2 * x_size + 1,
-                    y_pow)[-2 * x_size + 1:]
+                             2 * series_size + 1)),
+                    2 * series_size + 1)[-2 * series_size + 1:]
     else:
         return mult(x,
-                    add_global(mult(a, b, 2 * x_size + 1, y_pow),
+                    add_global(mult(a, b, 2 * series_size + 1),
                         mult(mult(minus_one,
                                   c,
-                                  2 * x_size + 1,
-                                  y_pow),
+                                  2 * series_size + 1),
                              d,
-                             2 * x_size + 1,
-                             y_pow)),
-                    2 * x_size + 1,
-                    y_pow)[-2 * x_size + 1:]
+                             2 * series_size + 1)),
+                    2 * series_size + 1)[-2 * series_size + 1:]
