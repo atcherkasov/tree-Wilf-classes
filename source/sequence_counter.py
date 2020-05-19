3aec9f2a438b8e012bd3017f1c38a3ff3463f0c2
atcherkasov <atcherkasov@hotmail.com>
2020-05-19 23:08:58 +0300

    fix

--- source/sequence_counter.py
+++ source/sequence_counter.py
@@ -4,12 +4,10 @@ def sequence_num(directory_path: str):
     different_series = set()
     for file_name in all_files:
         file = open(directory_path + '/' + file_name, 'r')
-        series = file.readlines()[1]
+        series = file.readlines()[2]
         different_series.add(series)
     print(len(different_series))
 
 
 if __name__ == '__main__':
-    sequence_num('../rubbish_files/output_8_75')
-    # hyp_test('output_files/long_data/series_8_111.txt')
-    # hyp_test('rubbish_files/short_nice_series_10_13.txt')
+    sequence_num('../output_files/Av_10_257')
